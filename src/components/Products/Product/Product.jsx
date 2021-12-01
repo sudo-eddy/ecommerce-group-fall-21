49f0f5e1a933b613b7e3ca4c2f8d2964d7800d20
pthom4 <pthom4@uis.edu>
2021-12-01 09:06:24 -0600

    UI Look feel changes

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -18,7 +18,7 @@ export const Product = ({ product, onAddToCart }) => {
                        {product.price.formatted_with_symbol}
                    </Typography>
                </div>
-               <Typography dangerouslySetInnerHTML={{__html: product.description}} variant="body2" color="textSecondary"/>
+               <Typography dangerouslySetInnerHTML={{__html: product.description}} variant="body1" color="textSecondary"/>
            </CardContent>
            <CardActions disableSpacing className={classes.cardActions}>
                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
